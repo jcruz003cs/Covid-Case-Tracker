c749ececdd3cc254b1952a1ae8b8071e963e2f80
Johnny Cruz <jcruz003cs@gmail.com>
2022-10-31 17:52:18 -0600

    commit with base level files

---
+++ src/Form.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (
+            <div>
+                <h1>Test Form</h1>
+            </div>
+        );
+    }
+}
+ 
+export default Form;
---
+++ src/Info.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Info extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (
+            <div>
+            <h1>Test Info</h1>
+            </div>
+        );
+    }
+}
+ 
+export default Info;
