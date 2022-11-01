dd66df61f6fbee94626699789da8b0839e3f4e75
Johnny Cruz <jcruz003cs@gmail.com>
2022-10-31 19:47:43 -0600

    Form done just need css work, need to transform data in info

--- src/Form.js
+++ src/Form.js
@@ -2,14 +2,51 @@ import React, { Component } from "react";
 
 class Form extends Component {
     constructor(props) {
-        super(props);
+        super(props)
+        this.state = {
+            stateName: "",
+            date: "",
+            statesArray: [{"name":"Alabama","abbreviation":"AL"},{"name":"Alaska","abbreviation":"AK"},{"name":"Arizona","abbreviation":"AZ"},{"name":"Arkansas","abbreviation":"AR"},{"name":"California","abbreviation":"CA"},{"name":"Colorado","abbreviation":"CO"},{"name":"Connecticut","abbreviation":"CT"},{"name":"Delaware","abbreviation":"DE"},{"name":"Florida","abbreviation":"FL"},{"name":"Georgia","abbreviation":"GA"},{"name":"Hawaii","abbreviation":"HI"},{"name":"Idaho","abbreviation":"ID"},{"name":"Illinois","abbreviation":"IL"},{"name":"Indiana","abbreviation":"IN"},{"name":"Iowa","abbreviation":"IA"},{"name":"Kansas","abbreviation":"KS"},{"name":"Kentucky","abbreviation":"KY"},{"name":"Louisiana","abbreviation":"LA"},{"name":"Maine","abbreviation":"ME"},{"name":"Maryland","abbreviation":"MD"},{"name":"Massachusetts","abbreviation":"MA"},{"name":"Michigan","abbreviation":"MI"},{"name":"Minnesota","abbreviation":"MN"},{"name":"Mississippi","abbreviation":"MS"},{"name":"Missouri","abbreviation":"MO"},{"name":"Montana","abbreviation":"MT"},{"name":"Nebraska","abbreviation":"NE"},{"name":"Nevada","abbreviation":"NV"},{"name":"New Hampshire","abbreviation":"NH"},{"name":"New Jersey","abbreviation":"NJ"},{"name":"New Mexico","abbreviation":"NM"},{"name":"New York","abbreviation":"NY"},{"name":"North Carolina","abbreviation":"NC"},{"name":"North Dakota","abbreviation":"ND"},{"name":"Ohio","abbreviation":"OH"},{"name":"Oklahoma","abbreviation":"OK"},{"name":"Oregon","abbreviation":"OR"},{"name":"Pennsylvania","abbreviation":"PA"},{"name":"Rhode Island","abbreviation":"RI"},{"name":"South Carolina","abbreviation":"SC"},{"name":"South Dakota","abbreviation":"SD"},{"name":"Tennessee","abbreviation":"TN"},{"name":"Texas","abbreviation":"TX"},{"name":"Utah","abbreviation":"UT"},{"name":"Vermont","abbreviation":"VT"},{"name":"Virginia","abbreviation":"VA"},{"name":"Washington","abbreviation":"WA"},{"name":"West Virginia","abbreviation":"WV"},{"name":"Wisconsin","abbreviation":"WI"},{"name":"Wyoming","abbreviation":"WY"}]
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
-    state = {  }
+    handleChange(event){
+        console.log(event.target.value)
+        const {name, value} = event.target
+        this.setState({
+            [name]:value
+        })
+    }
+    handleSubmit(event){
+        event.preventDefault()
+        const temp = []
+        temp.push(this.state.stateName)
+        temp.push(this.state.date)
+        window.localStorage.setItem('form', JSON.stringify(temp))
+        window.location.href = '#/info'
+    }
     render() { 
         return (
-            <div>
-                <h1>Test Form</h1>
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>State: 
+                        <select name="stateName" value={this.state.value} onChange = {this.handleChange}>
+                            <option>-- Select a State --</option>
+                            {this.state.statesArray.map((option) => (
+                            <option key={option.abbreviation} value={option.name}>
+                            {option.name}
+                        </option>))}
+                        </select>
+                    </label>
+                    <br/>
+                    <label> Date: 
+                        <input type = "date" name="date" value={this.state.value} onChange = {this.handleChange}/>
+                    </label>
+                    <br/>
+                    <input type = "submit" value = "Submit" />
+                </div>
+            </form>
         );
     }
 }
--- src/Info.js
+++ src/Info.js
@@ -3,9 +3,22 @@ import React, { Component } from "react";
 class Info extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: []
+        }
     }
-    state = {  }
+    componentDidMount(){
+        const lInfo = localStorage.getItem("form")
+        const lSInfo = JSON.parse(lInfo)
+        const url = `https://api.covid19api.com/country/united-states/status/confirmed?from=${lSInfo[1]}T00:00:00Z&to=${lSInfo[1]}T10:00:00Z&province=${lSInfo[0]}`
+        fetch(url)
+        .then((response) => response.json())
+        .then(data => {this.setState({data:data})})
+    }
+
+
     render() { 
+        console.log(this.state.data)
         return (
             <div>
             <h1>Test Info</h1>
