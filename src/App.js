cd3040b6b57b00ce7d33930c5c085a8aff4050c7
Johnny Cruz <jcruz003cs@gmail.com>
2022-10-31 18:24:08 -0600

    possible fix using HashRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import React from "react"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {HashRouter as HashRouter, Routes, Route} from "react-router-dom"
 import Form from "./Form"
 import Info from './Info'
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path = "/Covid-Case-Tracker" element = {<Form/>}/>
           <Route path = "/info" element = {<Info/>}/>
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
